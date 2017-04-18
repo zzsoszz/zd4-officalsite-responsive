e07e92f32a90e009b6da71cc94abca4d5677e141
hyq2015 <18030638805@163.com>
2017-04-18 14:33:36 +0800

    update

--- js/main.js
+++ js/main.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
 			$(this).addClass("active");
 		}
 	});
+	
+	// var prevWidth = element.style.width
+	// element.style.width = 'auto'
+	// var endWidth = getComputedStyle(element).width
+	// element.style.width = prevWidth
+	// element.offsetWidth // force repaint
+	// element.style.transition = 'width .5s ease-in-out'
+	// element.style.width = endWidth
+	// element.addEventListener('transitionend', function transitionEnd(event) {
+	// 	if (event.propertyName == 'width') {
+	// 		element.style.transition = ''
+	// 		element.style.width = 'auto'
+	// 		element.removeEventListener('transitionend', transitionEnd, false)
+	// 	}
+	// }, false)
+	
+
 });
